ef2babe1152b43367fbdb3bdbc7cf9c5e82e4973
kingarthurwashere <arthurnyasango1@outlook.com>
2024-11-15 00:41:13 +0200

    Initial commit

---
+++ lib/const/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:super_store/imports.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color kBlue = Color(0xff005DFF);
+  static const kGreen = Colors.green;
+}
---
+++ lib/const/raw_string.dart
@@ -0,0 +1,9 @@
+class RawString {
+  RawString._();
+  static String appName = 'Super Store';
+  static String appDescription = 'Flutter E-commerce App using REST API.';
+  static String gitHubRepo = 'https://github.com/Kingarthurwashere/Super-Store-Ecommerce-App-using-REST-Api-in-Flutter';
+  static String emailId = 'arthurnyasango1@outlook.com';
+  static String dummyEmail = 'superstore@gmail.com';
+  static String appLogoURL = 'https://ui-avatars.com/api/?name=Super+Store';
+}
